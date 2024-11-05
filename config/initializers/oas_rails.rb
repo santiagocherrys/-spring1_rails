f9462457ff153def4dc8967e6f4ebabeffe7b5e3
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-11-05 13:05:48 -0500

    delete oasis cord

--- config/initializers/oas_rails.rb
+++
@@ -1,5 +0,0 @@
-# config/initializers/oas_rails.rb
-OasRails.configure do |config|
-  # Set the base URL for your API here.
-  config.server_url = Rails.env.production? ? 'https://spring1-rails.onrender.com' : 'http://localhost:3000'
-end
